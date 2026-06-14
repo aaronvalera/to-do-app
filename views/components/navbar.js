11828bf3688e1b5c702f21d2f588de814303ac66
Aaron Valera <aaronrvalera@gmail.com>
2026-06-14 13:06:21 -0400

    Dynamic navbar buttons and add styles

--- views/components/navbar.js
+++ views/components/navbar.js
@@ -1,28 +1,60 @@
 // SELECTORS
 const navbar = document.getElementById("navbar");
+
 // NAVBAR COMPONENT
 const createNavBarHome = () => {
+    const path = window.location.pathname;
+    let desktopBtns = ``;
+    let mobileBtns = ``;
+    if(path === "/" || path === "/index.html") {
+        desktopBtns = `
+        <a href="/login/" class="w-24 text-center py-2 px-4 rounded-3xl font-bold bg-blue-400 hover:bg-blue-500 transition ease-in-out duration-300">Log In</a>
+        <a href="/signup/" class="w-24 text-center py-2 px-4 rounded-3xl font-bold bg-blue-700 hover:bg-blue-800 transition ease-in-out duration-300">Sign Up</a>
+    `;
+        mobileBtns = `
+        <a href="/login/" class="w-24 text-center py-2 px-4 rounded-3xl font-bold bg-blue-400 hover:bg-blue-500 transition ease-in-out duration-300">Log In</a>
+        <a href="/signup/" class="w-24 text-center py-2 px-4 rounded-3xl font-bold bg-blue-500 hover:bg-blue-600 transition ease-in-out duration-300">Sign Up</a>
+    `;
+    } else if(path.includes("signup")) {
+        desktopBtns = `
+        <a href="/" class="w-24 text-center py-2 px-4 rounded-3xl font-bold bg-blue-400 hover:bg-blue-500 transition ease-in-out duration-300">Home</a>
+        <a href="/login/" class="w-24 text-center py-2 px-4 rounded-3xl font-bold bg-blue-500 hover:bg-blue-600 transition ease-in-out duration-300">Log In</a>
+    `;
+        mobileBtns = `
+        <a href="/" class="w-24 text-center py-2 px-4 rounded-3xl font-bold bg-blue-400 hover:bg-blue-500 transition ease-in-out duration-300">Home</a>
+        <a href="/login/" class="w-24 text-center py-2 px-4 rounded-3xl font-bold bg-blue-500 hover:bg-blue-600 transition ease-in-out duration-300">Log In</a>
+    `;
+    } else if(path.includes("login")) {
+        desktopBtns = `
+        <a href="/" class="w-24 text-center py-2 px-4 rounded-3xl font-bold bg-blue-400 hover:bg-blue-500 transition ease-in-out duration-300">Home</a>
+        <a href="/signup/" class="w-24 py-2 px-4 rounded-3xl font-bold text-center bg-blue-500 hover:bg-blue-600 transition ease-in-out duration-300">Sign Up</a>
+    `;
+        mobileBtns = `
+        <a href="/" class="w-24 text-center py-2 px-4 rounded-3xl font-bold bg-blue-400 hover:bg-blue-500 transition ease-in-out duration-300">Home</a>
+        <a href="/signup/" class="w-24 py-2 px-4 rounded-3xl font-bold text-center bg-blue-500 hover:bg-blue-600 transition ease-in-out duration-300">Sign Up</a>
+    `;
+    }
     navbar.innerHTML = `
     <div class="relative flex justify-between items-center h-16 px-4 mx-auto max-w-7xl">
-        <a href="./" class="font-bold text-xl text-black">ToDoApp</a>
+        <a href="/" class="font-bold text-xl text-black">ToDoApp</a>
         <!-- Mobile -->
         <svg id="mobile-menu-btn" xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-list md:hidden p-1 rounded-xl text-black cursor-pointer hover:bg-gray-200 transition ease-in-out duration-300" viewBox="0 0 16 16">
         <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
         </svg>
         <div id="mobile-menu" class="hidden absolute flex-col justify-center items-center w-48 gap-4 top-16 right-4 py-5 rounded-xl text-white bg-gray-100 shadow-md md:hidden z-1">
-            <a href="/login/" class="py-2 px-4 w-24 rounded-3xl font-bold text-center bg-blue-400 hover:bg-blue-500 transition ease-in-out duration-300">Log In</a>
-            <a href="/signup/" class="py-2 px-4 w-24 rounded-3xl font-bold text-center bg-blue-500 hover:bg-blue-600 transition ease-in-out duration-300">Sign Up</a>
+            ${mobileBtns}
         </div>
         <!-- Desktop -->
         <div class="hidden md:flex flex-row px-2.5 gap-4 text-white">
-        <a href="/login/" class="py-2 px-4 rounded-3xl font-bold bg-blue-400 hover:bg-blue-500 transition ease-in-out duration-300">Log In</a>
-        <a href="/signup/" class="py-2 px-4 rounded-3xl font-bold bg-blue-700 hover:bg-blue-800 transition ease-in-out duration-300">Sign Up</a>
+            ${desktopBtns}
         </div>
     </div>
 `;
 };
+
 createNavBarHome();
 
+// SELECTORS
 const navbarListBtn = document.getElementById("mobile-menu-btn");
 const mobileMenu = document.getElementById("mobile-menu");
 
