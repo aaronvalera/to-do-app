f6296661d504b3ccda0087f4042f692985c08dca
Eileen A Valera R <eavalera19@gmail.com>
2026-06-14 18:36:53 -0400

    part 3,4,5 backend login

--- views/login/index.js
+++ views/login/index.js
@@ -1,18 +1,20 @@
-const emailInput = document.querySelector('#email');
-const passwordInput = document.querySelector('#password');
-const form = document.querySelector('#form');
+const emailInput = document.querySelector("#email");
+const passwordInput = document.querySelector("#password");
+const form = document.querySelector("#form");
+const errorText = document.querySelector("#error-text");
 
-form.addEventListener('submit', async e => {
-    e.preventDefault();
-    try {
-        const user = {
-            email: emailInput.value,
-            password: passwordInput.value
-        }
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  try {
+    const user = {
+      email: emailInput.value,
+      password: passwordInput.value,
+    };
 
-        await axios.post('/api/login', user);
-        console.log(user);
-    } catch (error) {
-        console.log(error);
-    }
+    await axios.post("/api/login", user);
+    window.location.pathname = `/todos/`;
+  } catch (error) {
+    console.log(error);
+    errorText.innerHTML = error.response.data.error;
+  }
 });
