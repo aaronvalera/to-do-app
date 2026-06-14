66041f10382216653de26ff3a07d5cb0705fa59f
Aaron Valera <aaronrvalera@gmail.com>
2026-06-14 00:02:16 -0400

    Modify navbar: mobile buttons' color

--- views/components/navbar.js
+++ views/components/navbar.js
@@ -9,13 +9,13 @@ const createNavBarHome = () => {
         <svg id="mobile-menu-btn" xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-list md:hidden p-1 rounded-xl text-black cursor-pointer hover:bg-gray-200 transition ease-in-out duration-300" viewBox="0 0 16 16">
         <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
         </svg>
-        <div id="mobile-menu" class="hidden absolute flex-col justify-center items-center w-48 gap-4 top-16 right-4 py-5 rounded-xl bg-gray-100 shadow-md md:hidden z-1">
-            <a href="/login/" class="py-2 px-4 w-24 rounded-3xl font-bold text-center hover:bg-gray-200 transition ease-in-out duration-300">Log In</a>
+        <div id="mobile-menu" class="hidden absolute flex-col justify-center items-center w-48 gap-4 top-16 right-4 py-5 rounded-xl text-white bg-gray-100 shadow-md md:hidden z-1">
+            <a href="/login/" class="py-2 px-4 w-24 rounded-3xl font-bold text-center bg-blue-400 hover:bg-blue-500 transition ease-in-out duration-300">Log In</a>
             <a href="/signup/" class="py-2 px-4 w-24 rounded-3xl font-bold text-center bg-blue-500 hover:bg-blue-600 transition ease-in-out duration-300">Sign Up</a>
         </div>
         <!-- Desktop -->
         <div class="hidden md:flex flex-row px-2.5 gap-4 text-white">
-        <a href="/login/" class="py-2 px-4 rounded-3xl font-bold bg-blue-300 hover:bg-blue-400 transition ease-in-out duration-300">Log In</a>
+        <a href="/login/" class="py-2 px-4 rounded-3xl font-bold bg-blue-400 hover:bg-blue-500 transition ease-in-out duration-300">Log In</a>
         <a href="/signup/" class="py-2 px-4 rounded-3xl font-bold bg-blue-700 hover:bg-blue-800 transition ease-in-out duration-300">Sign Up</a>
         </div>
     </div>
