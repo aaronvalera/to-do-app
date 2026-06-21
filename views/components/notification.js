60bfb75b22a867936ff663baec1b875e339d55cb
Aaron Valera <aaronrvalera@gmail.com>
2026-06-21 13:25:57 -0400

    Refactor code: notification

--- views/components/notification.js
+++ views/components/notification.js
@@ -7,15 +7,6 @@ export const displayNotification = (isError, message) => {
             <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293z"/>
         </svg>
     `;
-    } else {
-        notificationIcon = `
-        <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="blue" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
-            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
-        </svg>
-    `;
-    }
-              
-    if(isError) {
         div.innerHTML = `
         <div class="flex justify-center flex-col items-center gap-4 bg-slate-200 max-w-xs w-60 py-12 px-4 mx-8 font-extrabold rounded-lg text-white border border-gray-400 shadow-lg">
             ${notificationIcon}
@@ -24,6 +15,11 @@ export const displayNotification = (isError, message) => {
         </div>
     `;
     } else {
+        notificationIcon = `
+        <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="blue" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
+            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+        </svg>
+    `;
         div.innerHTML = `
         <div class="flex justify-center flex-col items-center gap-4 bg-slate-200 max-w-xs w-60 py-12 px-4 mx-8 font-extrabold rounded-lg text-white border border-gray-400 shadow-lg text-center">
             ${notificationIcon}
