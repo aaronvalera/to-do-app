6df3e10c3116a97cba1df7af4898bf37349b2076
Aaron Valera <aaronrvalera@gmail.com>
2026-06-21 14:02:33 -0400

    Fix typos to make login work

--- controllers/login.js
+++ controllers/login.js
@@ -6,49 +6,40 @@ const User = require("../models/user"); // (Pendiente) Importará el modelo de d
 loginRouter.post("/", async (req, res) => {
   //se define una ruta HTTP POST en la raíz del módulo. Usa 'async' porque adentro ejecutará tareas asíncronas (promesas).
   const { email, password } = req.body; //desestructura el cuerpo de la petición (req.body) para extraer directamente el correo y la contraseña enviados por el cliente.
-
   try {
     // intentamos ejecutar el flujo normal del login. si alguna línea aquí adentro falla, el código no rompe el servidor, sino que salta directo al catch
-    const userExist = await user.findOne({ email }); // busca de forma asíncrona en la base de datos un único usuario que coincida con el email recibido.
-
+    const userExist = await User.findOne({ email }); // busca de forma asíncrona en la base de datos un único usuario que coincida con el email recibido.
     if (!userExist) {
       // si la base de datos no devolvió ningún registro (userExist es null), significa que el correo no está registrado.
-      return res.status(400).json({ error: "Usuario no encontrado" }); //estado 400 (Bad Request)
+      return res.status(400).json({ error: "User not found." }); //estado 400 (Bad Request)
     }
-
     if (!userExist.verified) {
       // si la propiedad 'verified' del usuario es falsa (el usuario no ha verificado su cuenta).
-      return res.status(400).json({ error: "email no verificado" });
+      return res.status(400).json({ error: "Email not verified." });
     }
-
-    const itsCorrect = await bcrypt.compare(password, userExist.password); // compara asíncronamente la contraseña en texto plano con el hash guardado en la base de datos.
-
+    const itsCorrect = await bcrypt.compare(password, userExist.passwordHash); // compara asíncronamente la contraseña en texto plano con el hash guardado en la base de datos.
     if (!itsCorrect) {
       // si las passwords no coinciden
-      return res.status(400).json({ error: "Contraseña incorrecta" });
+      return res.status(400).json({ error: "Incorrect password." });
     }
-
     const userForToken = {
       // crea el objeto con los datos del usuario (en este caso el ID) que queremos meter dentro del token.
       id: userExist.id, //guardamos el ID único porque es el dato que usará el servidorpara identificar quién es este usuario.
     };
-
     const token = jwt.sign(userForToken, process.env.ACCESS_TOKEN_SECRET, {
       // jwt.sign junta los datos del usuario con nuestra clave secreta para generar el token firmado.
       expiresIn: "1d", //definimos cuando expira el token
     });
-
     res.cookie("accessToken", token, {
       // guarda el token generado dentro de una cookie en el navegador del cliente.
       maxAge: 24 * 60 * 60 * 1000, // define la duración de la cookie (1 día).
       secure: process.env.NODE_ENV === "production", // si está en producción, la cookie solo se transmite mediante conexiones seguras HTTPS.
       httpOnly: true, // pprotege la cookie prohibiendo que sea accesible mediante scripts de JavaScript en el frontend (evita ataques XSS).
     });
-
     return res.sendStatus(200); // responde exitosamente con un estado 200, notificando al frontend que el login fue correcto.
   } catch (error) {
     // captura cualquier fallo inesperado del servidor o de la base de datos.
-    return res.status(500).json({ error: "Error interno del servidor" }); // Responde con un estado 500 para no romper la aplicación.
+    return res.status(500).json({ error: "Internal server error."}); // Responde con un estado 500 para no romper la aplicación.
   }
 });
 
--- views/login/index.js
+++ views/login/index.js
@@ -3,9 +3,9 @@ const passwordInput = document.querySelector("#password"); // busca en el html e
 const form = document.querySelector("#form"); // selecciona el formulario completo desde el html para poder controlar cuándo se envía.
 const errorText = document.querySelector("#error-text"); // busca el espacio o la etiqueta de texto en el html donde mostraremos los mensajes de error si algo sale mal.
 
-form.addEventListener("submit", async (e) => {
+form.addEventListener("submit", async (event) => {
   // activa un escuchador para que en el momento en que el usuario le dé al botón de enviar (submit), se ejecute esta función asíncrona.
-  e.preventDefault(); // evita que la página se recargue
+  event.preventDefault(); // evita que la página se recargue
   try {
     // abre el bloque donde intentaremos hacer la petición al servidor si todo va bien.
     const user = {
@@ -13,13 +13,11 @@ form.addEventListener("submit", async (e) => {
       email: emailInput.value, // toma el texto exacto que se escribió en el campo de correo.
       password: passwordInput.value, // toma el texto exacto que se escribió en el campo de la contraseña.
     };
-
     await axios.post("/api/login", user); // usa la librería axios para enviar los datos del objeto 'user' de forma asíncrona al servidor mediante un método post, y espera a que el servidor responda.
     window.location.pathname = `/todos/`; // si el servidor responde que todo está bien, esta línea redirecciona automáticamente al usuario a la pantalla de sus tareas o la página principal.
   } catch (error) {
     // si el servidor devuelve un error (por ejemplo, contraseña incorrecta), el código salta directamente a este bloque de seguridad.
     console.log(error); // muestra el error detallado en la consola del navegador para que el desarrollador pueda revisar qué falló.
-    errorText.innerHTML = error.response.data.error;
     errorText.innerHTML = error.response.data.error; // usa la librería axios para buscar dentro de la carpeta del fallo (.response), abrir los datos que envió el servidor (.data) y sacar el texto del error (.error) para ponerlo en la pantalla.
   }
 });
