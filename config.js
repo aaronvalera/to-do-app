182d966cdd0b9ca35f16021f9da2f2ddfaac3837
Aaron Valera <aaronrvalera@gmail.com>
2026-07-06 14:01:09 -0400

    Fix production link

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const PAGE_URL = process.env.NODE_ENV === "production"
-    ? "placeholder"
+    ? "https://to-do-app-zuqb.onrender.com"
     : "http://localhost:3000";
 
 const MONGO_URI = process.env.NODE_ENV === "production"
